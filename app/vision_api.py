0cf49001d08fef7dcc47c687f9ded8f60ef1fe3e
David Schoch <david.schoch@zuehlke.com>
2021-03-14 13:06:56 +0100

    storage client

---
+++ app/vision_api.py
@@ -0,0 +1,55 @@
+from google.cloud.vision import AnnotateImageResponse, Feature, Image, ImageAnnotatorClient, Likelihood
+
+# TODO exception handling
+
+LIKELIHOODS = Likelihood
+VISION_FEATURES = [
+    {'type_': Feature.Type.FACE_DETECTION},
+    {'type_': Feature.Type.IMAGE_PROPERTIES},
+    {'type_': Feature.Type.LABEL_DETECTION},
+    {'type_': Feature.Type.LANDMARK_DETECTION},
+    {'type_': Feature.Type.LOGO_DETECTION},
+    {'type_': Feature.Type.OBJECT_LOCALIZATION},
+    {'type_': Feature.Type.SAFE_SEARCH_DETECTION},
+    {'type_': Feature.Type.TEXT_DETECTION},
+    {'type_': Feature.Type.WEB_DETECTION}
+]
+
+
+class ImageAnnotator:
+    def __init__(self):
+        self._annotator_client = None
+
+    def annotate_from_content(self, content: bytes):
+        image = Image(content=content)
+        return self._annotate(image)
+
+    def annotate_from_url(self, url: str):
+        image = Image(source={'image_uri': url})
+        return self._annotate(image)
+
+    def _annotate(self, image: Image) -> str:
+        self._build_client()
+        request = self._build_request(image)
+        response = self._annotator_client.annotate_image(request)
+        return AnnotateImageResponse.to_json(response)
+
+    def _build_client(self):
+        if self._annotator_client is None:
+            self._annotator_client = ImageAnnotatorClient()
+
+    def _build_request(self, image: Image) -> dict:
+        return {
+            'image': image,
+            'features': VISION_FEATURES
+        }
+
+
+if __name__ == '__main__':
+    annotator = ImageAnnotator()
+
+    with open(r"C:\Users\dsch\OneDrive\Bilder\_Titelbild.jpg", 'rb') as file:
+        content = file.read()
+        image = Image(content=content)
+        result = annotator.annotate_from_content(content)
+        print(result)
